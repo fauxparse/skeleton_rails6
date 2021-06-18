7953dbc910d0adf999adc0866638f194ead66e70
Matt Powell <fauxparse@gmail.com>
2021-06-19 11:19:36 +1200

    move style-loader to environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require('@rails/webpacker');
 
+environment.loaders
+  .get('sass')
+  .use.splice(0, 1, { loader: 'style-loader', options: {} });
+
 const erb = require('./loaders/erb');
 environment.loaders.prepend('erb', erb);
 
